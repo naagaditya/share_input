0fff3e57cb0454ad2d222cf043427732bbdb088f
Aditya Naag <adityanaag91@gmail.com>
2021-12-26 08:28:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,14 +65,14 @@ const joinChannel =  async () => {
 
 const initDbConnection = () => {
   var firebaseConfig = {
-    apiKey: "AIzaSyDdzhAHhMQeAo4egpTbj3K-JiuV_InAFkU",
+    apiKey: "apiKey",
     authDomain: "fir-rtc-8e4b2.firebaseapp.com",
     databaseURL: "https://fir-rtc-8e4b2.firebaseio.com",
     projectId: "fir-rtc-8e4b2",
     storageBucket: "fir-rtc-8e4b2.appspot.com",
-    messagingSenderId: "159303557727",
-    appId: "1:159303557727:web:2ae59edac452b77736b1ea",
-    measurementId: "G-FWJYK5VF1B"
+    messagingSenderId: "messageSenderId",
+    appId: "appId",
+    measurementId: "mesID"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
